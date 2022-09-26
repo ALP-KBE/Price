862283f6e6d21efa794e9347d6bf04390489262c
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-09-26 15:56:58 +0200

    add handling of products

--- src/main/java/ALP/KBEPrice/Controller/PriceController.java
+++ src/main/java/ALP/KBEPrice/Controller/PriceController.java
@@ -1,98 +1,20 @@
 package ALP.KBEPrice.Controller;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import ALP.RabbitMessage;
 import ALP.KBEPrice.Model.Component;
 import ALP.KBEPrice.Model.Product;
-import ALP.KBEPrice.RabbitMQ.RabbitMQSender;
-
-import static java.lang.Thread.sleep;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
 
 @RestController
 public class PriceController {
-    
-    private static String returnMessage;
-    @Autowired
-    private RabbitTemplate template;
-    @Autowired
-    private RabbitMQSender rabbitMQSender;
 
-    // @PostMapping("/price")
-    // public String calculatePrice(@RequestBody Product product) {
-    //     returnMessage = null;
-    //     System.out.println("sending message");
-    //     rabbitMQSender.send(new RabbitMessage("getComponents", product));
-    //     System.out.println("message sent");
-    //     while (returnMessage == null) {
-    //         try {
-    //             System.out.println("und wir warten");
-    //             sleep(10);
-    //         } catch (InterruptedException e) {
-    //             e.printStackTrace();
-    //         }
-    //     }
-    //     String rMessageCopy = returnMessage;
-    //     return rMessageCopy;
-    // }
-
-    public static void handle(String string) {
-        returnMessage = string;
-    }
-    
-    public void handle(Serializable serializable) {
-        boolean b1 = serializable instanceof LinkedHashMap;
-        boolean b2 = serializable instanceof ArrayList;
-        System.out.println("lhm: " + b1);
-        System.out.println("al: " + b2);
-        if (serializable instanceof LinkedHashMap) {
-            LinkedHashMap<?, ?> linkedHashMap = (LinkedHashMap<?, ?>) serializable;
-            // deserialising:
-            Product product = new Product((String) linkedHashMap.get("name"));
-            if(linkedHashMap.get("components") instanceof LinkedHashMap){
-                LinkedHashMap<String, ArrayList<LinkedHashMap<String, String>>> liHaMa = (LinkedHashMap<String, ArrayList<LinkedHashMap<String, String>>>) linkedHashMap
-                        .get("components");
-                for (String keyString : liHaMa.keySet()) {
-                    ArrayList<LinkedHashMap<String, String>> components = (ArrayList<LinkedHashMap<String, String>>) linkedHashMap
-                            .get(liHaMa.get(keyString));
-                    List<Component> comps = new LinkedList<>();
-                    for (LinkedHashMap<String, String> lhm : components) {
-                        Component comp = new Component();
-                        lhm.forEach((k, v) -> comp.set(k, v));
-                        comps.add(comp);
-                    }
-                    product.setComponents(comps);
-                    getPrice(product);
-                    //send back
-                }
-            }
-            else if(linkedHashMap.get("components") instanceof ArrayList){
-                ArrayList<LinkedHashMap<String, String>> components = (ArrayList<LinkedHashMap<String, String>>) linkedHashMap
-                            .get("components");
-                    List<Component> comps = new LinkedList<>();
-                    for (LinkedHashMap<String, String> lhm : components) {
-                        Component comp = new Component();
-                        lhm.forEach((k, v) -> comp.set(k, v));
-                        comps.add(comp);
-                    }
-                    product.setComponents(comps);
-                    getPrice(product);
-                    //send back
-                }
-                System.out.println(product.toString());
-            }
-
-            System.out.println("done");
+    public static Product setPrice(Product product) {
+        Product p = new Product();
+        p.setAdditionalInfo(product.getAdditionalInfo());
+        p.setName(product.getName());
+        p.setComponents(product.getComponents());
+        p.setPrice(getPrice(product));
+        return p;
     }
 
     public static float getPrice(Product product) {
--- src/main/java/ALP/KBEPrice/RabbitMQ/RabbitMQReceiver.java
+++ src/main/java/ALP/KBEPrice/RabbitMQ/RabbitMQReceiver.java
@@ -1,11 +1,17 @@
 package ALP.KBEPrice.RabbitMQ;
 
-import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ALP.RabbitMessage;
 import ALP.KBEPrice.Controller.PriceController;
@@ -16,16 +22,46 @@ import ALP.KBEPrice.Model.Product;
 public class RabbitMQReceiver {
     @Autowired
     PriceController priceController;
+    @Autowired
+    RabbitMQSender sender;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @RabbitHandler
-    public void receiver(String string) {
-        System.out.println("message angekommen " + string);
-        PriceController.handle(string);
+    public void receiver(RabbitMessage message) throws JsonMappingException, JsonProcessingException {
+        if (message.getType().equals("getPrice")) {
+            Product product = objectMapper.readValue((String) message.getValue(),
+                    new TypeReference<Product>() {
+                    });
+            product = PriceController.setPrice(product);
+            RabbitMessage rabbitMessage = new RabbitMessage("product", objectMapper.writeValueAsString(product));
+            sender.send(rabbitMessage);
+        } else if (message.getType().equals("getPrices")) {
+            System.out.println((String) message.getValue());
+            List<Product> products = objectMapper.readValue((String) message.getValue(),
+                    new TypeReference<List<Product>>() {
+                    });
+            List<Product> returnProducts = new LinkedList<>();
+            for (Product product : products) {
+                Product prod = PriceController.setPrice(product);
+                returnProducts.add(prod);
+            }
+            RabbitMessage rabbitMessage = new RabbitMessage("product", "");
+            if(returnProducts.size() == 1){
+                rabbitMessage.setValue(returnProducts.get(0));
+            }
+            else {
+                String returnString = "[";
+                for (int i = 0; i < returnProducts.size(); i++) {
+                    if(i == returnProducts.size() - 1){
+                        returnString = returnString + returnProducts.get(i).toString() + "]";
+                    }
+                    else {
+                        returnString = returnString + returnProducts.get(i).toString() + ",";
+                    }
+                }
+                rabbitMessage.setValue(returnString);
+            }
+            sender.send(rabbitMessage);
     }
-    
-    @RabbitHandler
-    public void receiver(RabbitMessage message) {
-        priceController.handle(message.getValue());
-
     }
 }
